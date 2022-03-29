5766bc6b7d0c336eebbb8e3f78b0047ed9f95f65
thejakia1 <thejakia1@gmail.com>
2022-03-29 16:26:30 +0600

    banner added

---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className='bg-img'>
+                <div className="banner-text mt-5">
+                    <h1>Best Solution <br />to Study at Home</h1>
+                </div>
+                <div className="banner-btn">
+                    <button className='btn btn-success mt-3'>See Courses</button>
+                    <button className='btn btn-warning ms-3 mt-3'>Take Lesson</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
